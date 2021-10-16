763c0aac46e2536281e81df89bceed41152b4b6f
GonzaloZ1 <87137455+GonzaloZ1@users.noreply.github.com>
2021-10-16 20:12:13 -0300

    Se añadio la ventana de lista de las pizzas

---
+++ app/src/main/java/com/inacap/start/listaPizza.java
@@ -0,0 +1,14 @@
+package com.inacap.start;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class listaPizza extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_pizza);
+    }
+}
