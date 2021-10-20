61f6cf980b4aca1922c483ec49804ddd21575135
Luis <techicode@outlook.com>
2021-10-20 09:56:29 -0300

    add add cart action. fix png images

--- app/src/main/java/com/inacap/start/DetalleProducto.java
+++ app/src/main/java/com/inacap/start/DetalleProducto.java
@@ -2,13 +2,28 @@ package com.inacap.start;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DetalleProducto extends AppCompatActivity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalle_producto);
+
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(DetalleProducto.this, CarroCompra.class);
+                startActivity(intent);
+            }
+        });
     }
 }
